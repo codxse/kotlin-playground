3f95d59783a2b7bc9e597acb617810957cf949f3
Nadiar AS <codxse@gmail.com>
2021-11-19 19:06:49 +0700

    kotlin and java

---
+++ kotlin_for_android/src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    var tim: String = "Tim Cook"
+    println(tim)
+
+    var timSalary: Int = 32
+    var monthly: Int = timSalary * 30
+    println(monthly)
+}
